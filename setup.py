46ad852a157ce1dd9be174508981f6777cd9328e
agenteAND <44445475+agenteAND@users.noreply.github.com>
2020-07-02 15:34:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='djangorrules',
     version='0.1',
-    install_requires=["django-multiselectfield==0.1.12", "python-dateutil"],
+    install_requires=["django-multiselectfield==0.1.12", "python-dateutil", "django-select2"],
     python_requires='>=3.6'
 )
